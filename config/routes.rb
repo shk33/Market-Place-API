08c8bee763b630f71ffeb3d4cbfe139acec956c8
Miguel Coronel <m.coronel.seg@gmail.com>
2018-01-04 01:10:18 -0600

    Adds test for show user

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ require 'api_constraints'
 
 MarketPlaceApi::Application.routes.draw do
 
-  devise_for :users
+	devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
 
     scope module: :v1,  constraints: ApiConstraints.new(version: 1, default: true) do
-      # We are going to list our resources here
+    	resources :users, :only => [:show]
     end
   end
 
---
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe Api::V1::UsersController do
+	before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
+
+	describe "GET #show" do
+		before(:each) do
+			@user = FactoryGirl.create :user
+			get :show, id: @user.id, format: :json
+		end
+
+		it "returns the information about a reporter on a hash" do
+			user_response = JSON.parse(response.body, symbolize_names: true)
+			expect(user_response[:email]).to eql @user.email
+		end
+
+		it "should respond with a 200" do
+			respond_with 200
+		end
+	end
+
+end
